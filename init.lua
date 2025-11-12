90e6eef5b4454c9cdf29439d7d3af7ae5d108aea
GBegg <beggio.guido@gmail.com>
2025-11-12 06:56:16 +0100

    snippets with default engine

--- init.lua
+++ init.lua
@@ -13,15 +13,14 @@ vim.opt.tabstop        = 4
 vim.opt.shiftwidth     = 4
 vim.opt.ignorecase     = true
 vim.opt.smartcase      = true
-vim.opt.hlsearch       = true
 vim.opt.colorcolumn    = "80"
 vim.opt.clipboard      = "unnamedplus" -- sync vim and standard copy register
-vim.opt.scrolloff      = 999 -- cursor always mid-screen
+vim.opt.scrolloff      = 99 -- cursor always mid-screen
 vim.opt.virtualedit    = "block" -- visual mode past end of line
 vim.g.netrw_liststyle  = 3 -- default file explorer (Netrw). 0: base, 1: date, 3: tree
 -- pop-up window dimensions
 vim.opt.pumheight      = 3
-vim.opt.pummaxwidth    = 24
+vim.o.timeoutlen       = 300
 
 ------------------------------- [[remap land]] (M = meta = alt key) -------------------------------
 -- quick save
@@ -44,10 +43,10 @@ vim.keymap.set("n", "<C-n>", "<cmd>t.|<CR>")
 -- open default file explorer (Netrw).
 vim.keymap.set("n", "-", "<cmd>Explore<CR>")
 -- resize splits
-vim.keymap.set("n", "<C-Left>", "  <cmd>vertical   resize -2<CR>")
-vim.keymap.set("n", "<C-Right>", " <cmd>vertical   resize +2<CR>")
-vim.keymap.set("n", "<C-Down>", "  <cmd>horizontal resize -2<CR>")
-vim.keymap.set("n", "<C-Up>", "    <cmd>horizontal resize +2<CR>")
+vim.keymap.set("n", "<C-Left>",  "<cmd>vertical   resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical   resize +2<CR>")
+vim.keymap.set("n", "<C-Down>",  "<cmd>horizontal resize -2<CR>")
+vim.keymap.set("n", "<C-Up>",    "<cmd>horizontal resize +2<CR>")
 -- [C]ompilation [M]ode / [C]ompile [M]e
 -- set makeprg=... (escape spaces with \) ([] + q to go across errors)
 vim.keymap.set("n", "<leader>cm", "<cmd>make<CR>")
@@ -86,8 +85,6 @@ vim.keymap.set("n", "<leader>t", "<cmd>terminal<CR>A")
 vim.keymap.set("v", "<leader>n", ":norm ")
 -- remove trailing whitespace
 vim.api.nvim_create_user_command('TRimTrailingWhiteSpace', function() vim.cmd([[%s/\s\+$//e]]) end, {})
--- the most basic snippets ever
-vim.keymap.set("n", "<leader>l", 'A<CR>for (size_t i = 0; i < n; ++i){<CR>}<CR><Esc>kk0fn') ---- c for loop
 -- inverse of [J]oin
 vim.keymap.set("v", "<leader>j", ":substitute/ /<C-v><CR>/gc<CR>a")
 
@@ -128,6 +125,22 @@ vim.api.nvim_create_user_command(
 -- [A]lign on = (or other) sign
 vim.keymap.set('v', '<leader>a', ':Align<CR>', { silent = true })
 
+------------------------------- snippets ----------------------------------------
+-- https://boltless.me/posts/neovim-config-without-plugins-2025/
+local function _expand_snippet(trigger, body)
+    local c = vim.fn.nr2char(vim.fn.getchar(0))
+    -- Only accept "<C-]>" as a trigger key.
+    if c ~= "" then vim.api.nvim_feedkeys(trigger .. c, "i", true)
+    -- once expanded, move between "nodes" with <tab>
+    else vim.snippet.expand(body)
+    end
+end
+local function _snippet(trigger, body)
+    vim.keymap.set("ia", trigger, function() _expand_snippet(trigger, body) end, {})
+end
+_snippet("for",  "for (size_t ${1:i} = 0; $1 < ${2:n}; ++$1){\n\t$3\n}"     ) -- c for loop
+_snippet("main", 'def main():\n\t$1\n\nif __name__ == "__main__":\n\tmain()') -- python main
+
 ------------------------------- colorscheme: POND -------------------------------
 vim.opt.termguicolors = true
 vim.cmd [[ colorscheme default ]] -- also evening
@@ -135,6 +148,7 @@ vim.api.nvim_set_hl(0, "LineNr", {         fg = "NvimLightGrey4" })
 vim.api.nvim_set_hl(0, "pythonInclude", {  link = "Statement" })
 vim.api.nvim_set_hl(0, "pythonOperator", { link = "Statement" })
 vim.api.nvim_set_hl(0, "Type", {           fg = "Lime" })
+vim.api.nvim_set_hl(0, "String", {         fg = "Lime" })
 vim.api.nvim_set_hl(0, "Identifier", {     link = "@variable" }) -- properties, struct fields
 vim.api.nvim_set_hl(0, "Special", {        fg = "NvimLightBlue" })
 vim.api.nvim_set_hl(0, "Boolean", {        link = "Function" })
@@ -143,94 +157,3 @@ vim.api.nvim_set_hl(0, "PmenuSel", {       fg = "Black",          bg = "DarkYell
 vim.api.nvim_set_hl(0, "Search", {         link = "PMenu" })
 vim.api.nvim_set_hl(0, "CurSearch", {      link = "PmenuSel" })
 vim.api.nvim_set_hl(0, "StatusLine", {     link = "PMenu" })
-
--- vim.api.nvim_set_hl(0, "String", {         fg = "Lime" })
--- vim.api.nvim_set_hl(0, "Normal", {         fg = "NvimLightGrey2", bg = "Black" })
--- vim.api.nvim_set_hl(0, "Function", {       fg = "NvimLightBlue" })
--- -- [[ launch with --noplugin option to skip this section ]]
--- ------------------------------- here be plugins -------------------------------
--- vim.opt.winborder = "single"
--- vim.api.nvim_set_hl(0, "NormalFloat", { fg = NvimLightGrey2, bg = "Black" })
--- -- readable errors (LSP): see <leader>d below
--- vim.diagnostic.config({virtual_text = false, virtual_lines = false})
--- -- set up lazy vim for plugins (~/.local/share/nvim)
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
---     vim.fn.system({
---         "git", "clone", "--filter=blob:none",
---         "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
---     })
--- end
--- vim.opt.rtp:prepend(lazypath)
--- require("lazy").setup {
---     { -- Autocompletion engine
---         "hrsh7th/nvim-cmp", event = "InsertEnter",
---         config = function()
---             local cmp = require("cmp")
---             cmp.setup({
---                 completion = { completeopt = "menu,noinsert" , },
---                 mapping = cmp.mapping.preset.insert {
---                     ["<C-n>"] = cmp.mapping.select_next_item(),
---                     ["<C-p>"] = cmp.mapping.select_prev_item(),
---                     ["<C-y>"] = cmp.mapping.confirm {select = true},
---                     ["<CR>"]  = cmp.mapping.confirm {select = true},
---                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
---                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
---                 },
---                 sources = {
---                     {name = "buffer", }, {name = "path", },
---                     -- {name = "nvim_lsp", }, -- for a quiet lsp experience
---                 },
---                 -- the following ops keep the suggestion menu short and nice so the docs are not squashed
---                 window = {documentation = {max_height = 12, max_width = 60, border = "single",}}, --completion
---                 formatting = {
---                     fields = {"abbr", "kind"}, -- menu (source path)
---                     format = function(_, vim_item)
---                         vim_item.abbr = string.sub(vim_item.abbr, 1, 14)
---                         vim_item.kind = string.sub(vim_item.kind, 1, 4)
---                         return vim_item
---                     end
---                 },
---             })
---         end,
---     },
---     { -- LSP
---         "neovim/nvim-lspconfig",
---         dependencies = {
---             "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim",
---             "WhoIsSethDaniel/mason-tool-installer.nvim", "hrsh7th/cmp-nvim-lsp",
---         },
---         config = function()
---             vim.api.nvim_create_autocmd("LspAttach", {
---                 group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
---                 callback = function(event)
---                     -- These GLOBAL keymaps are created unconditionally when Nvim starts:
---                     -- "gra" is mapped in Normal and Visual mode to |vim.lsp.buf.code_action()|
---                     -- "grn" is mapped in Normal            mode to |vim.lsp.buf.rename()|
---                     -- "grr" is mapped in Normal            mode to |vim.lsp.buf.references()|
---                     -- "gri" is mapped in Normal            mode to |vim.lsp.buf.implementation()|
---                     -- "gO" is mapped in Normal             mode to |vim.lsp.buf.document_symbol()|
---                     -- CTRL-S is mapped in Insert           mode to |vim.lsp.buf.signature_help()|
---                     vim.keymap.set("n", "gd",        vim.lsp.buf.definition,    {buffer = event.buf})
---                     vim.keymap.set("n", "K",         vim.lsp.buf.hover,         {buffer = event.buf})
---                     vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {buffer = event.buf})
---                 end,
---             })
---             local servers = { pyright = {}, rust_analyzer = {}, clangd = {}, gopls = {}, }
---             require("mason").setup()
---             local ensure_installed = vim.tbl_keys(servers or {})
---             require("mason-tool-installer").setup {ensure_installed = ensure_installed,}
---             local caps = vim.lsp.protocol.make_client_capabilities()
---             caps = vim.tbl_deep_extend("force", caps, require("cmp_nvim_lsp").default_capabilities())
---             require("mason-lspconfig").setup { handlers = {
---                 function(server_name)
---                     local server = servers[server_name] or {}
---                     server.capabilities = vim.tbl_deep_extend("force", {}, caps, server.capabilities or {})
---                     -- disable semantic tokens to avoid color changes -- seems useless ...
---                     -- server.on_attach = function(client) client.server_capabilities.semanticTokensProvider = nil end
---                     require("lspconfig")[server_name].setup(server)
---                 end,
---             }, }
---         end,
---     },
--- }
