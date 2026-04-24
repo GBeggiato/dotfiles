7eb6e8907d4145c2e6512854164c9ed96275ff38
GBegg <beggio.guido@gmail.com>
2026-04-24 07:33:39 +0200

    add wallpapers

--- get.sh
+++ get.sh
@@ -7,3 +7,8 @@ cp ~/.config/nvim/init.lua init.lua
 # cp ~/.config/nvim/plugins.lua plugins.lua
 
 cp ~/.config/nvim/install.sh install.sh
+
+cp ~/desktop/media/imgs/skyline.PNG        skyline.PNG
+cp ~/desktop/media/imgs/skills_border.PNG  skills_border.PNG
+cp ~/desktop/media/imgs/skills.PNG         skills.PNG
+
