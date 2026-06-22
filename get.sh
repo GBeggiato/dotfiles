63a89a2e85eb1152a67cc66cc3fe2d6b8100194c
GBegg <beggio.guido@gmail.com>
2026-06-22 07:00:46 +0200

    back to simplicity, 'update' alias

--- get.sh
+++ get.sh
@@ -1,14 +1,12 @@
 #! /usr/bin/bash
 
-# bashrc
-# cp ~/.bashrc .bashrc.void
-# nvim
+cp ~/.bashrc .bashrc
+echo "fetched .bashrc"
+
 cp ~/.config/nvim/init.lua init.lua
 echo "fetched init.lua"
 
 # cp ~/.config/nvim/plugins.lua plugins.lua
-
-# cp ~/.config/nvim/install.sh install.sh
 
 # cp ~/desktop/media/imgs/skyline.PNG        skyline.PNG
 # cp ~/desktop/media/imgs/skills_border.PNG  skills_border.PNG
--- init.lua
+++ init.lua
@@ -1,67 +1,7 @@
 local load_plugins = true
 local FILE_TYPE = "FileType"
 
--- colorscheme -----------------------------------------------------------------
--- function-preproc, const-type, string, statement, comment
-local current_colorscheme = 7
-vim.api.nvim_create_user_command(
-    'ColoRefresh',
-    function(opts)
-        if current_colorscheme == 0 then
-            vim.cmd.colorscheme("default")
-            vim.api.nvim_set_hl(0, "Type",           {link = "DiagnosticWarn"})
-            vim.api.nvim_set_hl(0, "PreProc",        {link = "Identifier"})
-            vim.api.nvim_set_hl(0, "Constant",       {link = "Identifier"})
-        elseif current_colorscheme == 1 then
-            vim.cmd.colorscheme("lunaperche")
-            vim.api.nvim_set_hl(0, "Function",       {link = "PreProc"})
-            vim.api.nvim_set_hl(0, "Special",        {link = "Normal"})
-        elseif current_colorscheme == 2 then
-            -- poor man's gruber darker (https://github.com/rexim/gruber-darker-theme)
-            vim.cmd.colorscheme("quiet")
-            vim.api.nvim_set_hl(0, "Statement",      {fg = "Yellow"})
-            vim.api.nvim_set_hl(0, "String",         {fg = "LimeGreen"})
-            vim.api.nvim_set_hl(0, "Comment",        {fg = "DarkOrange"})
-            vim.api.nvim_set_hl(0, "@markup.raw",    {fg = "Blue"})
-            vim.api.nvim_set_hl(0, "Function",       {fg = "#9e95c7"}) -- wisteria
-            vim.api.nvim_set_hl(0, "Type",           {fg = "#95a99f"}) -- quartz
-            vim.api.nvim_set_hl(0, "PreProc",        {fg = "#95a99f"})
-            vim.api.nvim_set_hl(0, "Constant",       {fg = "#95a99f"})
-            vim.api.nvim_set_hl(0, "Number",         {link = "Normal"})
-        elseif current_colorscheme == 3 then
-            vim.cmd.colorscheme("habamax")
-            vim.api.nvim_set_hl(0, "Special", {link = "Normal"})
-        elseif current_colorscheme == 4 then
-            vim.cmd("colorscheme darkblue")
-            vim.api.nvim_set_hl(0, "MatchParen", {fg = "LimeGreen", bg="DarkGreen"})
-        elseif current_colorscheme == 5 then
-            vim.cmd("colorscheme zaibatsu")
-            vim.api.nvim_set_hl(0, "MatchParen", {fg = "LimeGreen", bg="DarkGreen"})
-        elseif current_colorscheme == 6 then
-            vim.cmd("colorscheme pablo")
-            vim.api.nvim_set_hl(0, "Special", {link = "Normal"})
-        elseif current_colorscheme == 7 then
-            vim.cmd("colorscheme quiet")
-            vim.api.nvim_set_hl(0, "String",    {fg = "NvimLightGreen"})
-            vim.api.nvim_set_hl(0, "Statement", {fg = "DarkOrange"})
-            vim.api.nvim_set_hl(0, "Function",  {fg = "DarkYellow"})
-            vim.api.nvim_set_hl(0, "PreProc",   {link = "Function"})
-            vim.api.nvim_set_hl(0, "Constant",  {fg = "#ff00af"})
-            vim.api.nvim_set_hl(0, "Type",      {fg = "#ff00af"})
-        elseif current_colorscheme == 8 then
-            vim.api.nvim_set_hl(0, "String",    {fg = "NvimLightGreen"})
-            vim.api.nvim_set_hl(0, "Statement", {fg = "DarkOrange", bold=true})
-            vim.api.nvim_set_hl(0, "Function",  {fg = "DarkOrange"})
-            vim.api.nvim_set_hl(0, "PreProc",   {link = "Function"})
-            vim.api.nvim_set_hl(0, "Constant",  {link = "MatchParen"})
-            vim.api.nvim_set_hl(0, "Type",      {link = "MatchParen"})
-        end
-        vim.api.nvim_set_hl(0, "SpecialComment", {link = "Comment"})
-        current_colorscheme = (current_colorscheme + 1) % 9
-    end,
-    {}
-)
-vim.cmd("ColoRefresh")
+vim.cmd.colorscheme("habamax")
 
 -- basic behaviour -------------------------------------------------------------
 vim.g.mapleader        = vim.keycode("<space>")
